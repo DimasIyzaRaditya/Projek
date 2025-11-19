8e622238a91b3399c1ecc96f06eb6cc9a853c802
Azizteknokrat <aziz_prayoga@teknokrat.ac.id>
2025-11-19 11:40:06 +0700

    menghapus field jumlah

---
+++ api/prisma/migrations/20251119043937_hapus_jumlah/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `jumlah` on the `Transaksi` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Transaksi" DROP COLUMN "jumlah";
