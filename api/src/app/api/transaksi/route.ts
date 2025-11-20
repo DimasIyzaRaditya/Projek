c52e3e2fd0c75f86bec935ef0757a937d4bb8d19
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 14:59:53 +0700

    menambahkan isi function dengan variable transaksi

--- api/src/app/api/transaksi/route.ts
+++ api/src/app/api/transaksi/route.ts
@@ -3,4 +3,16 @@ import { prisma } from '@/lib/prisma';
 
 // GET all transaksi
 export async function GET() {
+    const transaksi = await prisma.transaksi.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+          },
+        },
+        produk: true,
+      }
+    });
 }
