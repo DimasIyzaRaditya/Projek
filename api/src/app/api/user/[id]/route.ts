e2ca5ae3daf10ae6f9e08b5ef79e56d4a04ea097
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 15:44:22 +0700

    membuat try dan catch

--- api/src/app/api/user/[id]/route.ts
+++ api/src/app/api/user/[id]/route.ts
@@ -89,6 +89,7 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
+  try {
     const { id } = await params;
     await prisma.user.delete({
       where: { id: parseInt(id) },
@@ -98,4 +99,6 @@ export async function DELETE(
       { message: 'User deleted successfully' },
       { status: 200 }
     );
+  } catch (error: any) {
+  }
 }
