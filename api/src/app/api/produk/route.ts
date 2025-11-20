54480718f886dbc52dc1c71dec5242ac5ff4d8ac
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 14:35:06 +0700

    menambahkan order by secara ascending

--- api/src/app/api/produk/route.ts
+++ api/src/app/api/produk/route.ts
@@ -4,7 +4,9 @@ import { prisma } from '@/lib/prisma';
 // GET all produk
 export async function GET() {
     const produk = await prisma.produk.findMany({
+      orderBy: {
+        id: 'asc',
+      },
     });
-    return NextResponse.json({ data: produk });
 }
 
