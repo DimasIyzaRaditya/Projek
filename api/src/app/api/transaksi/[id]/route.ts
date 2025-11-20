7258fb5139d19d7ccef3f684dc04c60ba3d79482
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 15:09:24 +0700

    membuat function get

--- api/src/app/api/transaksi/[id]/route.ts
+++ api/src/app/api/transaksi/[id]/route.ts
@@ -1,2 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
+
+// GET transaksi by ID
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {}
