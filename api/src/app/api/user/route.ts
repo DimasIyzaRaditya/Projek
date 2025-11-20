359d2a62dfe27d6d348b8389e173b4022e66dd49
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 16:56:41 +0700

    membuat kondisi jika error

--- api/src/app/api/user/route.ts
+++ api/src/app/api/user/route.ts
@@ -49,5 +49,10 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json({ data: user }, { status: 201 });
   } catch (error: any) {
+    if (error.code === 'P2002') {
+      return NextResponse.json(
+        
+      );
+    }
   }
 }
