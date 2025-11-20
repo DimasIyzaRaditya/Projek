c1de053cfb854b977ba14cc5cb374853870c31b5
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 14:53:07 +0700

    menambahkan jika data produk tidak ada

--- api/src/app/api/produk/[id]/route.ts
+++ api/src/app/api/produk/[id]/route.ts
@@ -1,26 +1,29 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 // GET produk by ID
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
-    const { id } = await params;
-    const produk = await prisma.produk.findUnique({
-      where: { id: parseInt(id) },
-      include: {
-        transaksi: {
-          include: {
-            user: {
-              select: {
-                id: true,
-                name: true,
-                username: true,
-              },
+  const { id } = await params;
+  const produk = await prisma.produk.findUnique({
+    where: { id: parseInt(id) },
+    include: {
+      transaksi: {
+        include: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              username: true,
             },
           },
         },
       },
-    });
+    },
+  });
+  if (!produk) {
+    return NextResponse.json({ error: "Produk not found" }, { status: 404 });
+  }
 }
