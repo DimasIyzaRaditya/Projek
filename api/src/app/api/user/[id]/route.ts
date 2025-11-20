7ea7e2d0de62b56af2ac5c6615f37045a96d97d2
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-11-20 15:26:41 +0700

    menambahkan transaksi

--- api/src/app/api/user/[id]/route.ts
+++ api/src/app/api/user/[id]/route.ts
@@ -13,6 +13,11 @@ export async function GET(
         id: true,
         name: true,
         username: true,
+        transaksi: {
+          include: {
+            produk: true,
+          },
+        },
       },
     });
 }
