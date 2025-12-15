e7308909b4034931a7167b29975d928a83eb6de8
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:54:28 +0700

    buat produk untuk ditaarik

--- mobile/app/detail/[id]/index.tsx
+++ mobile/app/detail/[id]/index.tsx
@@ -1,6 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+interface Produk {
+  id: number
+  nama: string
+  harga: number
+  deskripsi?: string
+  rating?: number
+  downloads?: number
+}
+
 export default function ProductDetailPage() {
   return (
     <View>
