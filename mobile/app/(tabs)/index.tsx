9bafdcfcdc6da5444dabc86950ce8278491ed2af
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:01:11 +0700

    menambahkan inline style

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useRouter } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { useRouter } from "expo-router";
 
 export default function Landingpage() {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <View>
-      <Text>index</Text>
+    <View
+      style={{
+        paddingVertical: 60,
+        paddingHorizontal: 16,
+        backgroundColor: "#0a0a0a",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Index</Text>
     </View>
-  )
+  );
 }
