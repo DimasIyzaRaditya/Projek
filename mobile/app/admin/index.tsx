b1e78c6d3a6f1afce8f49dcf62010944a87ba90e
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:48:11 +0700

    buat state

--- mobile/app/admin/index.tsx
+++ mobile/app/admin/index.tsx
@@ -2,6 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function AdminPage() {
+  // buat state
+  const router = useRouter();
+  const [loading, setLoading] = React.useState(true);
   return (
     <View>
       <Text>index</Text>
