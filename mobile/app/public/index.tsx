6f0924883fdc889b073c03b2cccb542e3a030082
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:18:02 +0700

    menambahkan produk untuk di tampilkan

--- mobile/app/public/index.tsx
+++ mobile/app/public/index.tsx
@@ -1,6 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+interface Produk {
+  id: number;
+  nama: string;
+  harga: number;
+}
+
 export default function HomePage() {
   return (
     <View>
