995e05447d33a356322cc642e68b94aa00426cf2
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:28:48 +0700

    import tag dari react native

--- mobile/app/public/index.tsx
+++ mobile/app/public/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image, Pressable } from "react-native";
 import React, { useState } from "react";
 import { API_PRODUK } from "@/scripts/api";
 import { useRouter } from "expo-router";
