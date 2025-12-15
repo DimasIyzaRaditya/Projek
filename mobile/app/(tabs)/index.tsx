77e1895635f08da2836887c37bc487eb46ae2a5c
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 13:58:33 +0700

    membuat variable

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
 
-export default function LandingPage() {
+export default function Landingpage() {
+  const router = useRouter()
   return (
     <View>
       <Text>index</Text>
