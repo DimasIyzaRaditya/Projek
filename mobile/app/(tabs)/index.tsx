bb6803938d4883cb9559927f5b9d1474ce879e9a
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:01:58 +0700

    menambahkan kalimat

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -14,7 +14,29 @@ export default function Landingpage() {
         justifyContent: "center",
       }}
     >
-      <Text>Index</Text>
+      <Text
+        style={{
+          fontSize: 32,
+          fontWeight: "bold",
+          color: "#fafafa",
+          textAlign: "center",
+          marginBottom: 16,
+        }}
+      >
+        Marketplace File Digital
+      </Text>
+      <Text
+        style={{
+          fontSize: 16,
+          color: "#a3a3a3",
+          textAlign: "center",
+          marginBottom: 32,
+          lineHeight: 24,
+        }}
+      >
+        Koleksi pribadi template, ebook, source code, dan aset digital untuk
+        kebutuhan Anda
+      </Text>
     </View>
   );
 }
