c7b15ad72a2ffced40c9da8c5742f0795d59c567
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:56:54 +0700

    import state

--- mobile/app/detail/[id]/index.tsx
+++ mobile/app/detail/[id]/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 
 interface Produk {
   id: number
