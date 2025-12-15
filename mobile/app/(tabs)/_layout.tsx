b90100d11ca1e20aface8d54c4e034f1c2cbd364
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 13:55:03 +0700

    membuat tag stack

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,8 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 export default function PublicLayout() {
   return (
+    <Stack>
 
+    </Stack>
   )
 }
