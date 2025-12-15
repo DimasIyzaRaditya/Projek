cf093ec6b909ae3ef41cb1082498fba9e0aa6641
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 15:03:51 +0700

    buat useeffect

--- mobile/app/seller/index.tsx
+++ mobile/app/seller/index.tsx
@@ -6,6 +6,29 @@ export default function SellerPage() {
   // buat state
   const router = useRouter()
   const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      // import AsyncStorage from '@react-native-async-storage/async-storage'
+      // const userData = await AsyncStorage.getItem("user")
+      // if (!userData) {
+      //   router.push("/login")
+      //   return
+      // }
+
+      // const parsedUser = JSON.parse(userData)
+      // if (parsedUser.username.toLowerCase() === "admin") {
+      //   router.push("/admin")
+      //   return
+      // }
+
+      // setUser(parsedUser)
+      setLoading(false)
+    }
+
+    checkAuth()
+  }, [])
+
   return (
     <View>
       <Text>index</Text>
