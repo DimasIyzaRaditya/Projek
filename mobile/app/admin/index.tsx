3c534bf0111a325f634ee6d69f29016012ed3bec
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 14:51:49 +0700

    import scrollview

--- mobile/app/admin/index.tsx
+++ mobile/app/admin/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React, { useEffect } from 'react'
 import { useRouter } from 'expo-router';
 import AsyncStorage from "@react-native-async-storage/async-storage";
