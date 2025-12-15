e76a70d400549e00a4c65d63a81d07c3be56afae
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 15:02:36 +0700

    buat state

--- mobile/app/seller/index.tsx
+++ mobile/app/seller/index.tsx
@@ -2,6 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function SellerPage() {
+  // buat state
+  const router = useRouter()
+  const [loading, setLoading] = React.useState(true)
   return (
     <View>
       <Text>index</Text>
