255bc9e111de07b3a4dfe395c8f1a1242486d19c
DimasIyzaRaditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-12-15 15:00:18 +0700

    import icon

--- mobile/app/detail/[id]/index.tsx
+++ mobile/app/detail/[id]/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Alert, Pressable, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams, useRouter } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 interface Produk {
   id: number;
